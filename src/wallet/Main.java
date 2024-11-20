a217b56210c155bc2ff588a59617dabd86fbc65d
Fernanda <eygleen.avila@alu.ing.unlp.edu.ar>
2024-11-20 19:12:51 -0300

    sqlite

--- src/wallet/Main.java
+++ src/wallet/Main.java
@@ -1,5 +1,11 @@
 package wallet;
 
+import wallet.config.DataBaseConfig;
+
 public class Main {
 
+    public static void main(String[] args) {
+        DataBaseConfig dataBase = new DataBaseConfig();
+        dataBase.CreateDatabase();
+    }
 }
