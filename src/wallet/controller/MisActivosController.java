f971bcbaeedda3e61d5b77777533d7fc562da467
LuAcuna <acunalucia064@gmail.com>
2024-12-07 15:54:38 -0300

    intentoControllers

---
+++ src/wallet/controller/MisActivosController.java
@@ -0,0 +1,34 @@
+package wallet.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import wallet.Main; //??
+import wallet.model.dto.ActivosModel;
+import wallet.view.ModelActivos;
+import wallet.view.ActivosView;
+
+import wallet.view.MainApp; 
+
+public class MisActivosController {
+    private ActivosView view;
+    private ModelActivos model;
+    private MainApp main;
+
+
+    public ActivosController (ActivosView view, ModelActivos model, MainApp main){
+        this.view = view;
+        this.model = model;
+        this.main = main; 
+
+        view.getButton().addActionListener( new exportarCSV());
+
+    }
+
+    class exportarCSV implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //exportar
+        }
+    } 
+}
